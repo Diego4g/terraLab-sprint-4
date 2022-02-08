e6302e481a1106687458f93a707b6481779d09a3
Diego Ferreira <diegodejesusferreira2@gmail.com>
2022-02-08 00:11:04 -0300

    :white_check_mark: added test for create user

--- src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.spec.ts
@@ -1,5 +1,4 @@
-import { AppError } from "src/shared/errors/AppError";
-
+import { AppError } from "../../../../shared/errors/AppError";
 import { UsersRepositoryInMemory } from "../../repositories/in-memory/UsersRepositoryInMemory";
 import { CreateUserUseCase } from "./CreateUserUseCase";
 
@@ -11,11 +10,25 @@ describe("Create User", () => {
     usersRepositoryInMemory = new UsersRepositoryInMemory();
     createUserUseCase = new CreateUserUseCase(usersRepositoryInMemory);
   });
+
   it("should be able to create a new user", async () => {
     const user = await createUserUseCase.execute({
       email: "teste@teste.com",
       password: "teste",
     });
     expect(user).toHaveProperty("id");
+  });
+
+  it("should not be to create a user with email already exist", () => {
+    expect(async () => {
+      await createUserUseCase.execute({
+        email: "teste2@teste2.com",
+        password: "teste2",
+      });
+      await createUserUseCase.execute({
+        email: "teste2@teste2.com",
+        password: "teste2",
+      });
+    }).rejects.toBeInstanceOf(AppError);
   });
 });
